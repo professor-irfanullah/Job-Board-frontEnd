a6e6785c20e7b6677056904d64fd0c730d7c3bb0
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-04 08:16:08 +0500

    modifiy router for guarding seeker and employer routes explicitly

--- src/router/router.js
+++ src/router/router.js
@@ -38,7 +38,7 @@ const routes = [
     },
     {
         path: '/saved-jobs',
-        component: () => import('../views/seeker_profile/savedJobs.vue')
+        component: () => import('../views/seeker_profile//savedJobs.vue')
     },
     {
         path: '/:pathMatch(.*)*',
@@ -54,12 +54,12 @@ const router = createRouter(
 
 const publicPaths = ['/', '/login']
 const privatePaths = ['/dashboard', '/profile', '/saved-jobs', '/accountSetting', 'sign-out', '/applications']
+/**
 router.beforeEach(async (to, from, next) => {
     const isAuth = useAuthStore()
+    const role = isAuth?.user?.user?.role
 
     const isAuthenticated = await isAuth.userAuthStatus()
-    const role = isAuth?.user?.user?.role
-    console.log(role);
     const isPublic = publicPaths.includes(to.path)
     const isPrivate = privatePaths.includes(to.path)
     if (isAuthenticated && isPublic) {
@@ -69,6 +69,36 @@ router.beforeEach(async (to, from, next) => {
         return next('/home')
     }
     next()
+})
+     */
+router.beforeEach(async (to, from, next) => {
+    const beforeAuthRoutesForSeeker = ['/home', '/find-jobs', '/companies', '/resources']
+    const afterAuthRoutesForSeeker = ['/dashboard', '/profile', '/saved-jobs', '/applications', '/accountSetting']
+    const avoidAfterAuth = ['/', '/login']
+    const userStore = useAuthStore()
+    const role = userStore?.user?.user?.role
+    const isAuthenticated = await userStore.userAuthStatus()
+    // avoid to access login or register for employee
+    if (isAuthenticated && avoidAfterAuth.includes(to.path) && role === 'employee') {
+        return next('/employee_dashboard')
+    }
+    // avoid to access login or register for seeker
+    if (isAuthenticated && avoidAfterAuth.includes(to.path) && role === 'seeker') {
+        return next('/dashboard')
+    }
+
+
+    // prevent beforeAuthRoutesForSeeker 
+    if (!isAuthenticated && afterAuthRoutesForSeeker.includes(to.path)) {
+        return next('/home')
+    }
+
+    if (isAuthenticated && role === 'employee' && (beforeAuthRoutesForSeeker.includes(to.path) || afterAuthRoutesForSeeker.includes(to.path))) {
+        return next('/employee_route')
+    }
+
+    next()
+
 })
 
 export default router
