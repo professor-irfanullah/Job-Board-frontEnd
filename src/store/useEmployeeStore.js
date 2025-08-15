5c5ab4050f6423671e1f7a238f85cede5c6868a8
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-15 18:48:23 +0500

    add employee store using  pinia state management

---
+++ src/store/useEmployeeStore.js
@@ -0,0 +1,62 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+import { computed, ref } from "vue";
+export const useEmployeeStore = defineStore('employeeStore', () => {
+    const profile_url = 'http://localhost:3000/api/employee/insertProfile'
+    const fetchProfileUrl = 'http://localhost:3000/api/employee/fetchProfile'
+    const fetchEmployeeAllJobsUrl = 'http://localhost:3000/api/employee/fetchEmployeeAllJobs'
+    const profileInfo = ref()
+    const employeeJobsLength = ref(null)
+    const employeeProgressUrl = 'http://localhost:3000/api/employee/fetchProfilePercentage'
+    const allJobs = ref([])
+    const profile_percentage = ref(null)
+    const insertEmployeeProfile = async (param) => {
+        try {
+            const response = await axios.post(profile_url, {
+                company_description: param.description,
+                founded_year: param.founded_year,
+                headquarters_location: param.headquarters,
+                industry: param.industry,
+                company_name: param.name,
+                company_size: param.size
+            }, { withCredentials: true })
+            console.log(response);
+
+            return response.data
+        } catch (error) {
+            throw error
+        }
+    }
+    const fetchEmployeeProfile = async () => {
+        try {
+            const response = await axios.get(fetchProfileUrl, { withCredentials: true })
+            profileInfo.value = response.data.data[0]
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const fetchEmployeeAllJobs = async () => {
+        try {
+            const response = await axios.get(fetchEmployeeAllJobsUrl, { withCredentials: true })
+            employeeJobsLength.value = response.data.length
+            allJobs.value = response.data
+            return response.data
+        } catch (error) {
+            throw error
+        }
+    }
+    const fetchEmployeeProfileCompletionProgress = async () => {
+        try {
+            const response = await axios.get(employeeProgressUrl, { withCredentials: true })
+            profile_percentage.value = response.data[0]
+        } catch (error) {
+            throw error
+        }
+    }
+
+    const limitedJobs = computed(() => {
+        return allJobs.value.slice(0, 5)
+    })
+    return { insertEmployeeProfile, fetchEmployeeProfile, fetchEmployeeAllJobs, fetchEmployeeProfileCompletionProgress, profileInfo, employeeJobsLength, allJobs, limitedJobs, profile_percentage }
+})
