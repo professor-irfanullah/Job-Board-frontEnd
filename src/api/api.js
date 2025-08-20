404d99e3eec9026a2a27cae01084298fdccb337f
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-21 01:32:50 +0500

    fixing bug

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+
 const api = axios.create({
     baseURL: import.meta.env.PRODUCTION_URL, // uses .env or .env.development
     withCredentials: true // only if you need cookies (auth, sessions)
