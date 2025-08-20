06f70124723ed5071066bc269ad7c78c7c5f4873
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-20 11:22:21 +0500

    add extra code for the employe profile image if he/she has not uploaded a profile photo

--- src/views/employee_profile/employeeProfile.vue
+++ src/views/employee_profile/employeeProfile.vue
@@ -9,7 +9,7 @@
         <section
           class="profile&info flex items-center flex-wrap w600:gap-2 gap-1"
         >
-          <div class="imageIcon relative">
+          <div v-if="companyInfo?.company_logo_url" class="imageIcon relative">
             <div
               class="img w-[50px] h-[50px] w200:w-[100px] w200:h-[100px] rounded-full overflow-hidden border-2 w200:border-4"
             >
@@ -26,6 +26,47 @@
               <label for="profile">
                 <p
                   class="absolute bottom-0 right-0 bg-indigo-600 text-white p-2 rounded-full hover:bg-indigo-800 transition-all transform hover:scale-110 shadow-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+                    />
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                </p>
+                <input
+                  @change="handleLogoChange"
+                  type="file"
+                  class="hidden"
+                  id="profile"
+                  accept="image/jpg,image/png,image/jpeg"
+                />
+              </label>
+            </div>
+          </div>
+          <div
+            v-else
+            class="imageIcon relative w-[5rem] h-[5rem] flex justify-center items-center text-4xl bg-green-400/25 text-gray-200 font-bold rounded-full"
+          >
+            {{ companyInfo?.email?.charAt(0).toUpperCase() }}
+            <div v-if="isEditing" class="profile">
+              <label for="profile">
+                <p
+                  class="absolute top-10 right-0 bg-indigo-600 text-white p-2 rounded-full hover:bg-indigo-800 transition-all transform hover:scale-110 shadow-md cursor-pointer"
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
