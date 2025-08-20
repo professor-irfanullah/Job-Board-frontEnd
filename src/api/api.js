f774db2f0a7bc53bb2f5a4675dfd2dd4370daf50
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-21 00:50:23 +0500

    revert changes

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import env from 'dotenv/config'
 const api = axios.create({
-    baseURL: process.env.PRODUCTION_URL, // uses .env or .env.development
+    baseURL: import.meta.env.PRODUCTION_URL, // uses .env or .env.development
     withCredentials: true // only if you need cookies (auth, sessions)
 });
 export default api;
