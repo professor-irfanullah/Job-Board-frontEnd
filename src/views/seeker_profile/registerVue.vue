a01d8980758db38202dd3dd09fe2556fb4a46e91
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-21 12:20:07 +0500

    revert changes

--- src/views/seeker_profile/registerVue.vue
+++ src/views/seeker_profile/registerVue.vue
@@ -382,11 +382,14 @@ const handleSubmit = async () => {
     //   email: form.value.email,
     //   password: form.value.password,
     // });
-    const response = await api.post("/api/auth/register", {
-      user_name: form.value.username,
-      email: form.value.email,
-      password: form.value.password,
-    });
+    const response = await axios.post(
+      "authentication-production-53cf.up.railway.app/api/auth/register",
+      {
+        user_name: form.value.username,
+        email: form.value.email,
+        password: form.value.password,
+      }
+    );
     okMessage.value = response.data.message;
     errMsg.value = "";
     form.value.email = "";
