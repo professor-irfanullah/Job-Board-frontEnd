1f5b909d927bd038cd82f1f655029c6ba5479b71
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-21 13:04:33 +0500

    rename the env variable

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.backEndURL, // uses .env or .env.development
+    baseURL: import.meta.env.VITE_BACKEND_URL, // uses .env or .env.development
     withCredentials: true // only if you need cookies (auth, sessions)
 });
 export default api;
