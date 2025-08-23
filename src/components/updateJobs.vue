410b67c0986aa96ac46ee4ce6f6d25b6721b7f15
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-08-23 22:44:52 +0500

    lowecase the values of employment types

--- src/components/updateJobs.vue
+++ src/components/updateJobs.vue
@@ -155,7 +155,7 @@
               class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             >
               <option value="" disabled selected>Select employment type</option>
-              <option value="Full-time">Full-time</option>
+              <option value="full-time">Full-time</option>
               <option value="part-time">Part-time</option>
               <option value="contract">Contract</option>
               <option value="internship">Internship</option>
