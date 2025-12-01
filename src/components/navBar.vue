52dc73781e626f372c5bcce2de48e59624ac5d2a
Professor Irfan Ullah <irfanprofessor60@gmail.com>
2025-12-01 21:35:15 +0500

    modified the path for the router link to navigate to companies page

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -48,7 +48,7 @@
         <router-link
           v-else
           class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
-          to="/finalJobs"
+          to="/companies"
           active-class="text-gray-900"
           >Companies</router-link
         >
